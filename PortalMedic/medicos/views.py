963d1c18e0f8d3e931cf1aa020b2a6a8f8bb9bab
Oscar Farias <oscarfarias297@gmail.com>
2024-05-15 21:55:33 -0300

    corrección de errores en pk

--- PortalMedic/medicos/views.py
+++ PortalMedic/medicos/views.py
@@ -35,7 +35,7 @@ def medicos_modificar(request, medico_id=None):
         return render(request, "medicos/medicos_form.html", {"form": form})
 
 
-def medicos_create(request, pk):
+def medicos_create(request):
     if request.method == "POST":
         form = MedicosForm(request.POST)
         if form.is_valid():
