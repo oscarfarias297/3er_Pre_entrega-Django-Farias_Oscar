041e5addf459606680e2d964a6f24112f507d303
Oscar Farias <oscarfarias297@gmail.com>
2024-05-19 19:26:05 -0300

    se agrega JS al creador de Medicos

---
+++ PortalMedic/core/static/core/js/sweetAlert.js
@@ -0,0 +1,27 @@
+// sweetAlert.js
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('form');
+  form.addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevenir el envío del formulario para mostrar la alerta primero
+
+      Swal.fire({
+          title: '¿Estás seguro?',
+          text: "¡No podrás revertir esto!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Sí, guardar cambios'
+      }).then((result) => {
+          if (result.isConfirmed) {
+              Swal.fire(
+                  '¡Guardado!',
+                  'Los cambios han sido guardados.',
+                  'success'
+              ).then(() => {
+                  form.submit(); // Enviar el formulario después de la confirmación
+              });
+          }
+      });
+  });
+});
